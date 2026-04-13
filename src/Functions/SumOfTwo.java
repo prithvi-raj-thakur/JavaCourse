a954e4da188b90fc8cc15c551a7fd8a7f2db92d1
prithvi-raj-thakur <05rajprithvi@gmail.com>
2026-04-13 23:31:54 +0530

    sum of two using function

---
+++ src/Functions/SumOfTwo.java
@@ -0,0 +1,19 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class SumOfTwo {
+    public static int sum(int a , int b ){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the first number: ");
+        int a = sc.nextInt();
+        System.out.println("enter the second number: ");
+        int b = sc.nextInt();
+        int result = sum (a,b);
+        System.out.println("sum = " + result);
+    }
+}
