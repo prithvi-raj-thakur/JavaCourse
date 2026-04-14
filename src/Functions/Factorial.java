86d91b8da6df1035d44828280ed1aa545ebdb35e
prithvi-raj-thakur <05rajprithvi@gmail.com>
2026-04-14 23:37:09 +0530

    factorial using function done ...

---
+++ src/Functions/Factorial.java
@@ -0,0 +1,21 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static int factorial (int n){
+        int fact = 1;
+        for (int i = 1 ; i <=n ; i++){
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number: ");
+        int n = sc.nextInt();
+
+        System.out.println("factorial = " + factorial(n));
+    }
+}
