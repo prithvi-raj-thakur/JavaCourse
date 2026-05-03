700493ae8601257c89e0742541436264f7f1dc18
prithvi-raj-thakur <05rajprithvi@gmail.com>
2026-05-03 16:11:05 +0530

    array 1D sum code done

---
+++ src/test_codes/OneDarraySum.java
@@ -0,0 +1,24 @@
+package test_codes;
+
+import java.util.Scanner;
+
+public class OneDarraySum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        int sum = 0;
+        System.out.println("enter the elements: ");
+
+        for ( int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        System.out.println("the sum is = "+sum);
+    }
+}
