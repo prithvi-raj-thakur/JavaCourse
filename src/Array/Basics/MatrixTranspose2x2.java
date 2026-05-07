f8ebbddadd3123812620e5722089bcec89f35b12
prithvi-raj-thakur <05rajprithvi@gmail.com>
2026-05-07 22:02:39 +0530

    array transpose matrix 2x2 code done

---
+++ src/Array/Basics/MatrixTranspose2x2.java
@@ -0,0 +1,44 @@
+package Array.Basics;
+
+import java.util.Scanner;
+
+public class MatrixTranspose2x2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] A = new int[2][2];
+        int[][] T = new int[2][2];
+
+        System.out.println("Enter Matrix A (2x2):");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                A[i][j] = sc.nextInt();
+            }
+        }
+
+        // Transpose logic
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                T[j][i] = A[i][j];
+            }
+        }
+
+        // Print original matrix
+        System.out.println("Original Matrix:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // Print transpose matrix
+        System.out.println("Transpose Matrix:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(T[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
