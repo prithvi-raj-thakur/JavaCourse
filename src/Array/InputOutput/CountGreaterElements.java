58a85368bf98b0ad8722bb056b0ccf9c0b1b4a2a
prithvi-raj-thakur <05rajprithvi@gmail.com>
2026-05-21 23:49:26 +0530

    Program to count the number of element strictly x in a given array done

--- src/Array/InputOutput/CountGreaterElements.java
+++ src/Array/InputOutput/CountGreaterElements.java
@@ -5,6 +5,27 @@ import java.util.Scanner;
 public class CountGreaterElements {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
 
+        int x;
+        System.out.println("enter the value of x : ");
+        x= sc.nextInt();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if(arr[i]>x)
+                count ++;
+        }
+        if(count==0)
+            System.out.println("element not found..");
+        else{
+            System.out.printf("elements greater than %d is %d",x,count);
+        }
+        sc.close();
     }
 }
