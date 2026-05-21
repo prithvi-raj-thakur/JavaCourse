5bf9445381b1dfa781b513e761489ca475ad2373
prithvi-raj-thakur <05rajprithvi@gmail.com>
2026-05-21 23:43:20 +0530

    Program to count the number of element strictly x in a given array done

---
+++ src/Array/InputOutput/CountGreaterElements.java
@@ -0,0 +1,10 @@
+package Array.InputOutput;
+
+import java.util.Scanner;
+//count the number of elements srtictly greater than x.
+public class CountGreaterElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
