20588af3e4d277b4c2a8b266b6f9f622469cc502
prithvi-raj-thakur <05rajprithvi@gmail.com>
2026-07-06 22:36:01 +0530

    even odd count in array code done

---
+++ src/test_codes/EvenOddCount.java
@@ -0,0 +1,33 @@
+package test_codes;
+
+import java.util.Scanner;
+
+public class EvenOddCount {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        int even = 0, odd = 0;
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+            if (arr[i] % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        System.out.println("Number of Even Elements = " + even);
+        System.out.println("Number of Odd Elements = " + odd);
+
+        sc.close();
+    }
+}
