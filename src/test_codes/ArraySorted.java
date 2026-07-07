6ae60a3fd72a44e1ae557aaac47f3e7ede61923b
prithvi-raj-thakur <05rajprithvi@gmail.com>
2026-07-07 23:10:10 +0530

    sorted array check code done

---
+++ src/test_codes/ArraySorted.java
@@ -0,0 +1,37 @@
+package test_codes;
+
+import java.util.Scanner;
+
+public class ArraySorted {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean sorted = true;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                sorted = false;
+                break;
+            }
+        }
+
+        if (sorted)
+            System.out.println("Array is Sorted.");
+        else
+            System.out.println("Array is Not Sorted.");
+
+        sc.close();
+    }
+}
