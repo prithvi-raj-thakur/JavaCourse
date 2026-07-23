4bd851d9c4ccca0498f433f1204776b69672de04
prithvi-raj-thakur <05rajprithvi@gmail.com>
2026-07-23 12:28:21 +0530

    array list adding code done

---
+++ src/test_codes/ArrayListExample.java
@@ -0,0 +1,15 @@
+package test_codes;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list);
+    }
+}
