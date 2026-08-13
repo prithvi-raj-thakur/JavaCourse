b9ba9bc5b274013b382519138a1ed537f3337cb4
prithvi-raj-thakur <05rajprithvi@gmail.com>
2026-08-13 21:25:48 +0530

    array list reverse code done

---
+++ src/test_codes/ArrayListReverse.java
@@ -0,0 +1,23 @@
+package test_codes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListReverse {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+
+        System.out.println("Original ArrayList: " + list);
+
+        Collections.reverse(list);
+
+        System.out.println("Reversed ArrayList: " + list);
+    }
+}
