8399964e75a445e5536c5617825754ee4bfcdaef
prithvi-raj-thakur <05rajprithvi@gmail.com>
2026-08-19 16:27:08 +0530

    largest elememt from array (approach 2 ) code done

---
+++ src/Problems/Arrays/LargestElement02.java
@@ -0,0 +1,26 @@
+package Problems.Arrays;
+
+import java.util.Scanner;
+
+public class LargestElement02  {
+    static int Largest(int[] arr){
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max,arr[i]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the no of elements in array : ");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        System.out.println("enter the elements : ");
+        for (int i = 0; i < n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("the largest element is : "+Largest(arr));
+        sc.close();
+    }
+}
